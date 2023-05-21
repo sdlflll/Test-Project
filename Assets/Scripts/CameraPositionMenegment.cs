c0a6aca066f3487bf42963ac9db2f6d2e979ff3c
COPP-CC-12\CODDY <behiji5404@djpich.com>
2023-05-21 16:16:36 +0300

    part 2

--- Assets/Scripts/CameraPositionMenegment.cs
+++
@@ -1,9 +0,0 @@
-using System.Collections;
-using System.Collections.Generic;
-using UnityEngine;
-
-abstract class CameraPositionMenegment : MonoBehaviour
-{
-    public abstract void SetNewCameraPosition(Vector3 NewCameraPosition);
-
-}
