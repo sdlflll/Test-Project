6a4dd9556dcb49204161e7b38216ccd401143152
COPP-CC-12\CODDY <behiji5404@djpich.com>
2023-05-27 10:17:30 +0300

    reset

--- Assets/my/Scripts/Start/JumpOrRunComplite.cs
+++ Assets/my/Scripts/Start/JumpOrRunComplite.cs
@@ -1,9 +1,6 @@
 using DG.Tweening;
-using System.Collections;
-using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.Playables;
-using static Autodesk.Fbx.FbxEuler;
 
 public class JumpOrRunComplite : MonoBehaviour
 {
